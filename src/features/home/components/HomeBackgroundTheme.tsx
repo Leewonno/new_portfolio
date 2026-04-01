bb4a6b1a53720db31ecf61298354d013a4ac1fd1
Leewonno <dldnjssh123@naver.com>
2026-04-01 21:29:55 +0900

    [FIX] 배경 문제

--- src/features/home/components/HomeBackgroundTheme.tsx
+++ src/features/home/components/HomeBackgroundTheme.tsx
@@ -45,7 +45,7 @@ export function HomeBackgroundTheme({
 
   return (
     <div
-      className={`${className} fixed inset-x-0 top-0 h-dvh z-[-1] overflow-x-hidden animate-[circle-reveal_2s_ease-out_forwards] will-change-[clip-path] contain-[paint]`}
+      className={`${className} fixed inset-0 z-[-1] animate-[circle-reveal_2s_ease-out_forwards] will-change-[clip-path] contain-[paint]`}
       style={{ ...style, transform: "translateZ(0)" }}
     >
       {icon &&
